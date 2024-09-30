ed23d3418e49b3da6b8b9f8fa519965667b72f2a
George <georgekutsia@gmail.com>
2024-09-30 02:16:30 +0100

    welcome pizza navbar

--- src/components/navbar/navbar-pizza/NavbarPizzaComponent.jsx
+++ src/components/navbar/navbar-pizza/NavbarPizzaComponent.jsx
@@ -1,36 +1,28 @@
 import "./navbarPizza.css"
 function NavbarPizzaComponent() {
   return (
-    <>
+    <div className="pizza-box">
       <div className="gallery">
         <img
-          src="https://assets.codepen.io/8889025/Collabrorative.PNG"
+          src="https://res.cloudinary.com/dtv1oj9bq/image/upload/v1727655340/img01_welcome_dz9mtj.png"
           alt="a hot air balloon"
         />
         <img
-          src="https://assets.codepen.io/8889025/Adventurous.PNG"
+          src="https://res.cloudinary.com/dtv1oj9bq/image/upload/v1727655341/img20_welcome_xougf5.png"
           alt="adventurous"
         />
         <img
-          src="https://assets.codepen.io/8889025/Reliable.PNG"
+          src="https://res.cloudinary.com/dtv1oj9bq/image/upload/v1727658897/img42_welcome_bbierp.png"
           alt="a small boat"
         />
         <img
-          src="https://assets.codepen.io/8889025/Energetic.PNG"
+          src="https://res.cloudinary.com/dtv1oj9bq/image/upload/v1727655808/img30_welcome_dsezl2.png"
           alt="a forest"
         />
       </div>
       <br />
-      <a
-        href="https://nhsbsauk.sharepoint.com.mcas.ms/sites/MyHub/SitePages/Our-Purpose-Vision-and-Values.aspx"
-        target="_blank"
-        className="btn btn-primary btn-block"
-        role="button"
-        rel="noopener noreferrer"
-      >
-        Our Purpose, Vision and Values
-      </a>
-    </>
+
+    </div>
   );
 }
 
