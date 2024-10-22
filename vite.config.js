51b6a67ad0380bc5aacf8b257275f7f242bc9d6a
George <georgekutsia@gmail.com>
2024-10-22 15:56:05 +0100

    non se

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { monacoEditorPlugin } from 'vite-plugin-monaco-editor';
+// Importa el paquete completo de vite-plugin-monaco-editor
+import MonacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
   plugins: [
     react(),
-    monacoEditorPlugin({
+    // Usa el plugin de Monaco como el valor por defecto del paquete importado
+    MonacoEditorPlugin({
       languageWorkers: ['editorWorkerService', 'typescript', 'css', 'html', 'json'],  // Define los workers que necesitas
     })
   ],
@@ -15,9 +17,8 @@ export default defineConfig({
         target: 'https://api.jdoodle.com',
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
 });
-
