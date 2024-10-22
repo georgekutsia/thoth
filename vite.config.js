92cbe7b56c4a5cea74730ffcf654ac9910f4fd3a
George <georgekutsia@gmail.com>
2024-10-22 15:49:16 +0100

    hey monaco

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import MonacoEditorPlugin from 'vite-plugin-monaco-editor';
+import { monacoEditorPlugin } from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
   plugins: [
     react(),
-    MonacoEditorPlugin({
+    monacoEditorPlugin({
       languageWorkers: ['editorWorkerService', 'typescript', 'css', 'html', 'json'],  // Define los workers que necesitas
     })
   ],
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.jdoodle.com',  // Redirige a la API correcta de JDoodle
-        changeOrigin: true,  // Cambia el origen de la solicitud para evitar problemas de CORS
-        secure: true,  // Asegura que la conexión es HTTPS
-        rewrite: (path) => path.replace(/^\/api/, '')  // Quita el prefijo '/api' de la solicitud antes de enviarla
+        target: 'https://api.jdoodle.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
