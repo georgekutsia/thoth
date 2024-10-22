347c67a907a22477bf7cce8d0b0d032b3983e482
George <georgekutsia@gmail.com>
2024-10-22 13:00:05 +0100

    swiper ajustado

--- src/components/slider/ProgramacionSliderComponent.jsx
+++ src/components/slider/ProgramacionSliderComponent.jsx
@@ -23,31 +23,29 @@ export default function ProgramacionSliderComponent() {
     <NavigationSliderComponent slideTo={slideTo} fromData='indice' navData={programacionNotas}/>
       <Swiper className="mySwiper swiper-h" spaceBetween={50} pagination={{ clickable: true }} modules={[Pagination]} onSwiper={setSwiperRef} >
         <SwiperSlide>
-        <>
             <CardSlider fromData={programacionNotas.ejemplo} text='Programación (java)' />
             <div className='pdf-downloader'>
             <DpfDownloaderComponent text='Ir a la sala principal en Alexia '  url={"https://thepowerfp.alexiaclassroom.com/course/view.php?id=594&section=1"} nav/>   
             <DpfDownloaderComponent text='Programación didáctica Programación ' textPlus={"y sistemas de gestión de información"} url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/26093/mod_resource/content/1/Programacio%CC%81n%20dida%CC%81ctica%20Programacio%CC%81n.pdf"}/>       
             </div>
-        </>
         </SwiperSlide>
         <SwiperSlide>
         <Swiper className="mySwiper2 swiper-v" direction={'vertical'} spaceBetween={50} pagination={{ clickable: true }} modules={[Pagination]}>
-        <SwiperSlide>          
-          <CardSlider fromData={programacionNotas.basicJava} text='Básico Programación (java)' />
-            <div className='pdf-downloader'>
-            <DpfDownloaderComponent text='Inicio a la Programación' url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/26102/mod_resource/content/1/UD1.%20Inicio%20a%20la%20Programaci%C3%B3n.pdf"}/>
-            <DpfDownloaderComponent text='Configuración de Netbeans y XAMPP '  url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/29572/mod_resource/content/1/Netbeans%20%20XAMPP.pdf"}/>       
-            </div>
+              <SwiperSlide>          
+                <CardSlider fromData={programacionNotas.basicJava} text='Básico Programación (java)' />
+                  <div className='pdf-downloader'>
+                  <DpfDownloaderComponent text='Inicio a la Programación' url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/26102/mod_resource/content/1/UD1.%20Inicio%20a%20la%20Programaci%C3%B3n.pdf"}/>
+                  <DpfDownloaderComponent text='Configuración de Netbeans y XAMPP '  url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/29572/mod_resource/content/1/Netbeans%20%20XAMPP.pdf"}/>       
+                  </div>
+                  </SwiperSlide>
+              <SwiperSlide>          
+                <CardSlider fromData={programacionNotas.conditionalJava} text='Condicionales (java)' />
+                  <div className='pdf-downloader'>
+                  <DpfDownloaderComponent text='Inicio a la Programación' url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/26102/mod_resource/content/1/UD1.%20Inicio%20a%20la%20Programaci%C3%B3n.pdf"}/>
+                  <DpfDownloaderComponent text='Configuración de Netbeans y XAMPP '  url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/29572/mod_resource/content/1/Netbeans%20%20XAMPP.pdf"}/>       
+                  </div>
             </SwiperSlide>
-        <SwiperSlide>          
-          <CardSlider fromData={programacionNotas.conditionalJava} text='Condicionales (java)' />
-            <div className='pdf-downloader'>
-            <DpfDownloaderComponent text='Inicio a la Programación' url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/26102/mod_resource/content/1/UD1.%20Inicio%20a%20la%20Programaci%C3%B3n.pdf"}/>
-            <DpfDownloaderComponent text='Configuración de Netbeans y XAMPP '  url={"https://thepowerfp.alexiaclassroom.com/pluginfile.php/29572/mod_resource/content/1/Netbeans%20%20XAMPP.pdf"}/>       
-            </div>
-            </SwiperSlide>
-          </Swiper>
+        </Swiper>
             </SwiperSlide>
         <SwiperSlide>
           <Swiper className="mySwiper2 swiper-v" direction={'vertical'} spaceBetween={50} pagination={{ clickable: true }} modules={[Pagination]}>
